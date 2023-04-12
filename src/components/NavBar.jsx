1dc139b058a82a4ec157206029f06a148f0e567a
Cesar Pizarra <cpizarra002@gmail.com>
2023-04-13 06:01:30 +0800

    news home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
-import {BsJustify, BsXLg} from 'react-icons/bs'
-const NavBar = () => {
-  const [nav, setNav] = useState(false);
-  const handleNav = () =>{
-    setNav(!nav)
-  }
-  return (
-    <div className='w-full flex items-center justify-between px-[2rem] py-[3rem]'>
-        <h1 className='font-bold text-4xl'>W.</h1>
-            <ul className='hidden md:flex'>
-                <li className='ml-5'><a href="">Home</a></li>
-                <li className='ml-5'><a href="">New</a></li>
-                <li className='ml-5'><a href="">Popular</a></li>
-                <li className='ml-5'><a href="">Trending</a></li>
-                <li className='ml-5'><a href="">Categories</a></li>
-            </ul>
-           <div className='z-10 block md:hidden' onClick={handleNav}>
-            {!nav ? <BsXLg size={30} /> : <BsJustify size={30} />}
-  
-           </div>
-           <div className={!nav ? 'fixed right-0 top-0 w-[70%] h-full bg-white ease-in-out duration-500 '  : 'fixed right-[-100%] ease-in-out duration-500 h-full top-0'} >
-           <ul className='pt-36 uppercase text-center'>
-                <li className='p-4 text-lg'><a href="">New</a></li>
-                <li className='p-4'><a href="">Popular</a></li>
-                <li className='p-4 text-lg'><a href="">Home</a></li>
-                <li className='p-4 text-lg'><a href="">Trending</a></li>
-                <li className='p-4 text-lg'><a href="">Categories</a></li>
-            </ul>
-           </div>
-    </div>
-  )
-}
+import {BsJustify, BsXLg} from 'react-icons/bs';
 
-export default NavBar
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1300px] mx-auto px-4'>
+      <h1 className='w-full text-3xl font-bold text-black'>W.</h1>
+      <ul className='hidden text-black md:flex'>
+        <li className='p-4 text-lg'>Home</li>
+        <li className='p-4 text-lg'>Company</li>
+        <li className='p-4 text-lg'>Resources</li>
+        <li className='p-4 text-lg'>About</li>
+        <li className='p-4 text-lg'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <BsXLg size={30}/> : <BsJustify size={30} />}
+      </div>
+      <ul className={nav ? 'fixed left-0 top-0 w-[80%] h-full bg-white text-black ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+      
+           <li className='p-5 text-lg'><a href="">New</a></li>
+           <li className='p-5 text- text-lglg'><a href="">Popular</a></li>
+           <li className='p-5 text-lg'><a href="">Home</a></li>
+           <li className='p-5 text-lg'><a href="">Trending</a></li>
+           <li className='p-5 text-lg'><a href="">Categories</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
